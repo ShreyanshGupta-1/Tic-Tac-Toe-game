33268b8fdca79cec163ae671bd917586d6312aab
ShreyanshGupta-1 <sh.g87557@gmail.com>
2026-07-14 09:44:19 +0530

    Initial commit

---
+++ item.js
@@ -0,0 +1,73 @@
+let boxes=document.querySelectorAll(".box");
+let reset=document.querySelector("#reset");
+let newGameBtn=document.querySelector("#new-btn");
+let msgcont=document.querySelector(".msg-cont");
+let msg=document.querySelector("#msg");
+let turnO=true;
+const winPattern=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+boxes.forEach((box) => {
+    box.addEventListener("click",()=>{
+        if (turnO==true){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+        checkwinner();
+    });
+});
+
+const enablebtn=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+const disablebtn=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+const showwinner=(winner)=>{
+    msg.innerText=`Congratulation, Winner is ${winner}`;
+    msgcont.classList.remove("hide");
+    disablebtn();
+}
+const checkwinner=()=>{
+    for(let pattern of winPattern){
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+
+        if(pos1val !="" && pos2val !="" && pos3val !=""){
+            if(pos1val==pos2val && pos2val==pos3val){
+                console.log(pos1val,"is the winner");
+                showwinner(pos1val);
+            }
+        }
+    }
+}
+
+const resetGame=()=>{
+    turn0=true;
+    enablebtn();
+    msgcont.classList.add("hide");
+}
+const newGame=()=>{
+    turn0=true;
+    enablebtn();
+    msgcont.classList.add("hide");
+}
+newGameBtn.addEventListener("click",newGame);
+reset.addEventListener("click",resetGame);
